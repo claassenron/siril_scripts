e036726c777ec0dca9cf50a327fed11aed947909
Ron Claassen <claassen.ron@home.nl>
2026-03-11 08:54:41 +0100

    Drizzele

--- RC_OSC_PREPROCESSING.py
+++ RC_OSC_PREPROCESSING.py
@@ -245,7 +245,7 @@ class RcPreprocessingInterface(QMainWindow):
         container_child_1_layout.addWidget(process_group)
         
         # Bias group
-        bias_group = QGroupBox("Select Bias Path OR Master Dark File")
+        bias_group = QGroupBox("Select Bias Path OR Master Bias File")
         bias_layout = QVBoxLayout()
         bias_group.setLayout(bias_layout)
         
