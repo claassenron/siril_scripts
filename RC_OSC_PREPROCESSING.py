674e63c63e9b924915688c2e2dbccc6041bdafa3
Ron Claassen <claassen.ron@home.nl>
2026-06-11 16:54:18 +0200

    Update

--- RC_OSC_PREPROCESSING.py
+++ RC_OSC_PREPROCESSING.py
@@ -797,14 +797,18 @@ class RcPreprocessingInterface(QMainWindow):
             # Check if paths are selected
             if create_bias_var == True or create_dark_var == True:
                 if (process_path_var in (None, "") or not Path(process_path_var).is_dir()) or (create_bias_var == True and (bias_path_var in (None, "") or not Path(bias_path_var).is_dir())) or (create_dark_var == True and (dark_path_var in (None, "") or not Path(dark_path_var).is_dir())):
-                    missing_folders = []
+                    warning_messages = []
                     if process_path_var in (None, "") or not Path(process_path_var).is_dir():
-                        missing_folders.append("a valid process folder")
-                    if missing_folders:
+                        warning_messages.append("Select a valid process folder.")
+                    if create_bias_var == True and (bias_path_var in (None, "") or not Path(bias_path_var).is_dir()):
+                        warning_messages.append("Select a valid bias folder.")
+                    if create_dark_var == True and (dark_path_var in (None, "") or not Path(dark_path_var).is_dir()):
+                        warning_messages.append("Select a valid dark folder.")
+                    if warning_messages:
                         QMessageBox.warning(
                             self,
-                            "Select Required Folder",
-                            "Please select:\n- " + "\n- ".join(missing_folders),
+                            "Check Selections",
+                            "Please correct:\n- " + "\n- ".join(warning_messages),
                         )
                     if (process_path_var in (None, "") or not Path(process_path_var).is_dir()):
                         self.siril.log(
